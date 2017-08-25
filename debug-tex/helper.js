a2d5d62a0c3c4a3202dccb60bc66501bea388cb5
Shreevatsa R <shreevatsa.public@gmail.com>
2017-08-25 02:37:11 -0700

    some sort of hack

---
+++ debug-tex/helper.js
@@ -0,0 +1,25 @@
+function makeTableFromBuffer(text, start, loc, limit) {
+    let inner = '<table><tr>';
+    for (let c of text) {
+	inner += '<td>' + c + '</td>';
+    }
+    inner += '</tr>';
+    inner += '<tr><td colspan=';
+    inner += loc - start;
+    inner += '>';
+    inner += '<td>↑</td>';
+    inner += '</tr>';
+    inner += '</table>';
+
+    const elt = document.createElement('tt');
+    // elt.innerHTML = '<table><tr><td>e</td><td>x</td><td>p</td><td>a</td><td>n</td><td>d</td><td>a</td><td>f</td><td>t</td><td>e</td><td>r</td><td>.</td><td>t</td><td>e</td><td>x</td></tr><tr><td>↑</td><td colspan=15></td><td>↑</td></tr></table>';
+    elt.innerHTML = inner;
+    return elt;
+}
+
+function update() {
+    document.getElementById('replace1').appendChild(makeTableFromBuffer('expandafter.tex', 1, 17, 15));
+    document.getElementById('replace2').appendChild(makeTableFromBuffer('\\expandafter\\uppercase\\expandafter{a}%', 17, 57, 58));
+}
+
+update();
