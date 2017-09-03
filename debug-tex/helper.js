9d68edf9e82282f427e060b662e8580af79be65a
Shreevatsa R <shreevatsa.public@gmail.com>
2017-09-03 05:46:55 -0700

    move inner buffer-table to Vue

--- debug-tex/helper.js
+++ debug-tex/helper.js
@@ -1,7 +1,7 @@
 function makeTableFromBuffer(text, start, loc, limit) {
     let inner = '<table border=1><tr>';
     for (let c of text) {
-	inner += '<td>' + c + '</td>';
+        inner += '<td>' + c + '</td>';
     }
     inner += '</tr>';
     inner += '<tr><td colspan=';
@@ -11,22 +11,23 @@ function makeTableFromBuffer(text, start, loc, limit) {
     inner += '</tr>';
     inner += '</table>';
 
-    const elt = document.createElement('tt');
-    // elt.innerHTML = '<table><tr><td>e</td><td>x</td><td>p</td><td>a</td><td>n</td><td>d</td><td>a</td><td>f</td><td>t</td><td>e</td><td>r</td><td>.</td><td>t</td><td>e</td><td>x</td></tr><tr><td>↑</td><td colspan=15></td><td>↑</td></tr></table>';
-    elt.innerHTML = inner;
-    return elt;
+    return inner;
 }
 
-function update() {
-    // document.getElementById('replace1').appendChild(makeTableFromBuffer('expandafter.tex', 1, 17, 15));
-    document.getElementById('replace2').appendChild(makeTableFromBuffer('\\expandafter\\uppercase\\expandafter{a}%', 17, 57, 58));
-}
-
-update();
+// Make sure to get https://vuejs.org/js/vue.js and load it first
+Vue.component('buffer-table', {
+    props: ['text', 'start', 'loc', 'limit'],
+    render: function(createElement) {
+        return createElement('tt', {
+            domProps: {
+                innerHTML: makeTableFromBuffer(this.text, this.start, this.loc, this.limit),
+            },
+        });
+    }
+});
 
 var app = new Vue({
-  el: '#replace1',
+  el: '#full-table',
   data: {
-      replace1Html: makeTableFromBuffer('expandafter.tex', 1, 17, 15).outerHTML,
   }
 });
