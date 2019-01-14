339501c28a8b9d83bbd68a22083f95b059a63982
Shreevatsa R <shreevatsa.public@gmail.com>
2019-01-14 01:34:08 -0800

    for now, include the svgs, as slow as they are

--- program/include-all.js
+++ program/include-all.js
@@ -3,12 +3,12 @@ console.log('Running include-all');
 let d = document.getElementById('all');
 const numPages = 1383;
 function addPage(p) {
-    // <div id="0001"><img src="tex-0001.svg"/></div>
+    // <div id="0001"><img src="svgs/tex-0001.svg"/></div>
     let div = document.createElement('div');
     let idStr = p.toString().padStart(4, '0'); 
     div.id = idStr;
     let img = document.createElement('img');
-    img.src = "tex-" + idStr + ".svg";
+    img.src = "svgs/tex-" + idStr + ".svg";
     div.appendChild(img);
     d.appendChild(div);
     if (p < numPages) {
